ff799683896d8d8136f7ef8304361b82562488ae
nshimiyejm <nshimiyimanajean_1@live.com>
2017-12-04 01:13:21 -0500

    JMN
    Added Some changes and connected few activies going from login to home screen (created by Cristal)

---
+++ app/src/main/java/com/pme/sse/pme/DisplayHome.java
@@ -0,0 +1,13 @@
+package com.pme.sse.pme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_home);
+    }
+}
--- app/src/main/java/com/pme/sse/pme/MainActivity.java
+++ app/src/main/java/com/pme/sse/pme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pme.sse.pme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+    }
+
+    public void pmelogin(View view){
+        Intent startNewActivity = new Intent(this, DisplayHome.class);
+        startActivity(startNewActivity);
+
     }
 
     @Override
